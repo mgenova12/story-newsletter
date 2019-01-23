16ea9e8fd2e262ed741904ea148d433ad9cf4fd0
Marco <mgenova03@gmail.com>
2019-01-22 20:50:40 -0500

    add navbar and destroy action

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,26 +1,42 @@
 class StoriesController < ApplicationController
   
   def index
-
+    @stories = Story.all
   end
 
   def new
-
+    @story = Story.new
   end
 
   def create
+    employee = Story.new(story_params)
 
+    if employee.save
+      redirect_to "/stories"
+    end
   end
 
   def show 
-
+    @story = Story.find(params[:id])
   end
 
   def edit 
-
+    @story = Story.find(params[:id])
   end
 
   def update
+    story = Story.find(params[:id])
+
+    story.update(story_params)
+
+    redirect_to "/stories/#{story.id}"
+  end
+
+  def destroy
+    story = Story.find(params[:id])
+    story.destroy
+
+    redirect_to "/stories"
 
   end
 
@@ -28,7 +44,7 @@ class StoriesController < ApplicationController
   private 
 
   def story_params
-    
+    params.require(:story).permit(:title, :tag, :content)
   end
 
 end
